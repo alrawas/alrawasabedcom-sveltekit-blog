c4c02c6043e56e830326dbe2590ecc14f68cbf0b
alrawas <alrawas.abed@gmail.com>
2022-04-18 13:39:46 +0300

    install utterances

--- src/routes/blog/[post].svelte
+++ src/routes/blog/[post].svelte
@@ -1,84 +1,92 @@
 <!-- This file renders each individual blog post for reading. Be sure to update the svelte:head below -->
-
 <script context="module">
-  export const load = async ({ params }) => {
-    try {  
-      const post = await import(`../../lib/posts/${params.post}.md`)
+	export const load = async ({ params }) => {
+		try {
+			const post = await import(`../../lib/posts/${params.post}.md`);
 
-      return {
-        props: {
-          PostContent: post.default,
-          meta: { ...post.metadata, slug: params.post } 
-        }
-      }
-    } catch(error) {
-      return {
-        status: 404,
-        error: error.message
-      }
-    }
-  }
+			return {
+				props: {
+					PostContent: post.default,
+					meta: { ...post.metadata, slug: params.post }
+				}
+			};
+		} catch (error) {
+			return {
+				status: 404,
+				error: error.message
+			};
+		}
+	};
 </script>
-
 
 <script>
-  export let PostContent
-  export let meta
+	export let PostContent;
+	export let meta;
 
-  const { title, excerpt, date, updated, coverImage, coverWidth, coverHeight, categories } = meta
+	const { title, excerpt, date, updated, coverImage, coverWidth, coverHeight, categories } = meta;
 </script>
 
-
 <svelte:head>
-  <!-- Be sure to add your image files and un-comment the lines below -->
-  <title>{title}</title>
-  <meta name="twitter:card" content="summary_large_image">
-  <meta data-key="description" name="description" content="{excerpt}">
-  <meta property="og:type" content="article" />
-  <meta property="og:title" content={title} />
-  <meta name="twitter:title" content={title} />
-  <meta property="og:description" content={excerpt} />
-  <meta name="twitter:description" content={excerpt} />
-  <meta property="og:image" content="https://alrawasabed.com{coverImage}" />
-  <meta property="og:image:width" content={coverWidth} />
-  <meta property="og:image:height" content={coverHeight} />
-  <meta name="twitter:image" content="https://alrawasabed.com{coverImage}" />
+	<!-- Be sure to add your image files and un-comment the lines below -->
+	<title>{title}</title>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta data-key="description" name="description" content={excerpt} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={title} />
+	<meta name="twitter:title" content={title} />
+	<meta property="og:description" content={excerpt} />
+	<meta name="twitter:description" content={excerpt} />
+	<meta property="og:image" content="https://alrawasabed.com{coverImage}" />
+	<meta property="og:image:width" content={coverWidth} />
+	<meta property="og:image:height" content={coverHeight} />
+	<meta name="twitter:image" content="https://alrawasabed.com{coverImage}" />
 </svelte:head>
 
+<div>
+	<article class="post">
+		<!-- You might want to add an alt frontmatter attribute. If not, leaving alt blank here works, too. -->
+		<img
+			class="cover-image"
+			src={coverImage}
+			alt=""
+			style="aspect-ratio: {coverWidth} / {coverHeight};"
+			width={coverWidth}
+			height={coverHeight}
+		/>
 
-<article class="post">
-  <!-- You might want to add an alt frontmatter attribute. If not, leaving alt blank here works, too. -->
-  <img
-    class="cover-image"
-    src="{coverImage}"
-    alt=""
-    style="aspect-ratio: {coverWidth} / {coverHeight};"
-    width={coverWidth}
-    height={coverHeight}
-  />
+		<h1>{title}</h1>
 
-  <h1>{ title }</h1>
-  
-  <div class="meta">
-    <b>Published:</b> {date}
-    <br>
-    <b>Updated:</b> {updated}
-  </div>
-  
-  <svelte:component this={PostContent} />
+		<div class="meta">
+			<b>Published:</b>
+			{date}
+			<br />
+			<b>Updated:</b>
+			{updated}
+		</div>
 
-  {#if categories}
-    <aside class="post-footer">
-      <h2>Posted in: </h2>
-      <ul>
-        {#each categories as category}
-          <li>
-            <a href="/blog/category/{category}/">
-              { category }
-            </a>
-          </li>
-        {/each}
-      </ul>
-    </aside>
-  {/if}
-</article>
+		<svelte:component this={PostContent} />
+
+		{#if categories}
+			<aside class="post-footer">
+				<h2>Posted in:</h2>
+				<ul>
+					{#each categories as category}
+						<li>
+							<a href="/blog/category/{category}/">
+								{category}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</aside>
+		{/if}
+	</article>
+	<script
+		src="https://utteranc.es/client.js"
+		repo="https://github.com/alrawas/alrawasabedcom-sveltekit-blog"
+		issue-term="pathname"
+		theme="github-dark"
+		crossorigin="anonymous"
+		async>
+	</script>
+</div>
